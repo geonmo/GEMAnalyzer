d2437351f83acae5b3e69b4a288d491bfd2cce7c
Geonmo <ry840901@gmail.com>
2014-11-26 17:41:28 +0900

    Add configuration file for v6 and v7.

---
+++ digiAnalyzer/test/test_geom_v6_cfg.py
@@ -0,0 +1,44 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended2023MuonReco_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        'file:out_reco_v6.root'
+    )
+)
+
+process.demo = cms.EDAnalyzer('geomanalyzer',
+)
+
+process.source = cms.Source("EmptySource")
+process.TFileService = cms.Service("TFileService",
+ fileName = cms.string("ntuple_geom_v6.root"),
+)
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS3', '')
+
+# Automatic addition of the customisation function from Geometry.GEMGeometry.gemGeometryCustoms
+from Geometry.GEMGeometry.gemGeometryCustoms import custom_GE11_8and8partitions_v1 
+
+#call to customisation function custom_GE11_8and8partitions_v1 imported from Geometry.GEMGeometry.gemGeometryCustoms
+process = custom_GE11_8and8partitions_v1(process)
+
+# End of customisation functions
+
+
+process.p = cms.Path(process.demo)
